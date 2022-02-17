9f91353212abe9a9ae7754d9b0627b6f0466502e
Douglas Souza <douglassouzabjj@hotmail.com>
2022-02-17 14:03:18 -0300

    Corrige importaçao do arquivo css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-import './App.css';
-
 function App() {
   return (
     <>
