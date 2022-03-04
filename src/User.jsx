d45c3cda8ead289ad5312b16d838ae8427f27786
Douglas Souza <douglassouzabjj@hotmail.com>
2022-03-04 10:55:00 -0300

    Adiciona componentDIdMount

--- src/User.jsx
+++ src/User.jsx
@@ -1,51 +1,47 @@
 import React from 'react';
 
 class User extends React.Component {
-  constructor(props) {
-    super(props);
+  // 1
+  constructor() {
+    super();
 
     this.state = {
       user: null,
-      isLoading: true,
-    }
+    };
 
     this.fetchUser = this.fetchUser.bind(this);
   }
 
+  // 3
   componentDidMount() {
     this.fetchUser();
-  };
-
-  shouldComponentUpdate(nextProps, nextState) {
-    const { user: { registered: { age } } } = nextState;
-    return age > 10;
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-  };
-
-  componentWillUnmount() {
   }
+
+  // 4
+  shouldComponentUpdate(nextProps, nextState) { return true; }
+
+  // 5
+  componentDidUpdate(prevProps, prevState) {}
+
+  // 6
+  componentWillUnmount() {}
 
   async fetchUser() {
-    const response = await fetch('https://api.randomuser.me/');
+    const url = 'https://api.randomuser.me/';
+    const response = await fetch(url);
     const { results } = await response.json();
-    const [user] = results
-    this.setState({ user, isLoading: false });
+    const [user] = results; // results[0]
+    this.setState({ user });
   }
 
+  // 2
   render() {
-    const { user, isLoading } = this.state;
+    const { user } = this.state;
 
-    return (
-      isLoading
-        ? <div>Loading...</div>
-        : <div>
-          <h2>{ `${user.name.first} ${user.name.last}, ${ user.registered.age }` }</h2>
-          <p>{ user.email }</p>
-          <button type="button" onClick={ this.fetchUser }>Next User</button>
-        </div>
-    );
+    return (user
+      ? <div><h2>{ user.name.first }</h2></div>
+      : <div>User not found 404</div>
+    )
   }
 }
 
