ab9b259e4d21b72ba5266ccc961eb5bcbfad736f
Douglas Souza <douglassouzabjj@hotmail.com>
2022-03-09 17:58:48 -0300

    Adiciona personagens em EpisodeDetails

--- src/pages/EpisodeDeatails.jsx
+++ src/pages/EpisodeDeatails.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Loading from '../components/Loading';
@@ -10,9 +11,11 @@ class EpisodeDetails extends React.Component {
     this.state = {
       isLoading: true,
       episode: null,
+      characters: null,
     }
 
     this.fetchEpisode = this.fetchEpisode.bind(this);
+    this.getCharacters = this.getCharacters.bind(this);
   }
 
   componentDidMount() {
@@ -25,11 +28,27 @@ class EpisodeDetails extends React.Component {
     const response = await fetch(`https://rickandmortyapi.com/api/episode/${params.id}`);
     const episode = await response.json();
 
-    this.setState({ isLoading: false, episode });
+    this.setState(
+      { episode },
+      async () => {
+        await this.getCharacters();
+        this.setState({ isLoading: false });
+      }
+    );
+  }
+
+  async getCharacters() {
+    const { episode } = this.state;
+    const characters = await Promise.all(episode.characters.map(async (c) => {
+      const response = await fetch(c);
+      const { id, name } = await response.json();
+      return { id, name };
+    }));
+    this.setState({ characters });
   }
   
   render() {
-    const { isLoading, episode: e } = this.state;
+    const { isLoading, episode: e, characters } = this.state;
 
     return (
       <>
@@ -42,6 +61,18 @@ class EpisodeDetails extends React.Component {
                 <h2>{ e.name }</h2>
                 <h3>{ e.episode }</h3>
                 <p>{ `Aired on: ${ e.air_date }` }</p>
+                <h3>Featured characters:</h3>
+                <ul>
+                  {
+                    characters.map(({ id, name }) => (
+                      <Link key={ id } to={ `/characters/${id}` }>
+                        <li>
+                          { name }
+                        </li>
+                      </Link>
+                    ))
+                  }
+                </ul>
               </div>
             )
         }
