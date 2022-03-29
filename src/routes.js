d73b37df2328e03c655943785ea56f988a0521bf
Douglas Souza <douglassouzabjj@hotmail.com>
2022-03-29 09:01:27 -0300

    Adiciona componente de rotas

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={} />
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default Routes;
