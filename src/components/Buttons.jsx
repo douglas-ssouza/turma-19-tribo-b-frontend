7a60a915b3d3fc96b127533798b6bf9441a0672b
Douglas Souza <douglassouzabjj@hotmail.com>
2022-04-13 11:42:58 -0300

    Refatora componentes de classes para que sejam funcionais

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -2,30 +2,28 @@ import React from 'react';
 
 import pokemonContext from '../context/pokemonContext';
 
-class Buttons extends React.Component {
-  render() {
-    return (
-      <pokemonContext.Consumer>
-        {
-          ({ types, resetFilter, filterPokemons }) => (
-            <section>
-              <button type="button" onClick={ resetFilter }>All</button>
-              {
-                types.map((type) => (
-                  <button
-                    key={ type }
-                    onClick={ () => filterPokemons(type) }
-                  >
-                    { type }
-                  </button>
-                ))
-              }
-            </section>
-          )
-        }  
-      </pokemonContext.Consumer>
-    );
-  }
+function Buttons() {
+  return (
+    <pokemonContext.Consumer>
+      {
+        ({ types, resetFilter, filterPokemons }) => (
+          <section>
+            <button type="button" onClick={ resetFilter }>All</button>
+            {
+              types.map((type) => (
+                <button
+                  key={ type }
+                  onClick={ () => filterPokemons(type) }
+                >
+                  { type }
+                </button>
+              ))
+            }
+          </section>
+        )
+      }  
+    </pokemonContext.Consumer>
+  );
 }
 
 export default Buttons;
--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -2,23 +2,21 @@ import React from 'react';
 
 import pokemonContext from '../context/pokemonContext';
 
-class Pokemon extends React.Component {
-  render() {
-    return (
-      <pokemonContext.Consumer>
-        {
-          ({ selectedPokemon: { image, name, type, averageWeight } }) => (
-            <div>
-              <img src={ image } alt={ `${name} sprite` } />
-              <h2>{ name }</h2>
-              <h3>{ type }</h3>
-              <p>{ `Average Weight: ${averageWeight.value}${averageWeight.measurementUnit}` }</p>
-            </div>
-          )
-        }
-      </pokemonContext.Consumer>
-    );
-  }
+function Pokemon() {
+  return (
+    <pokemonContext.Consumer>
+      {
+        ({ selectedPokemon: { image, name, type, averageWeight } }) => (
+          <div>
+            <img src={ image } alt={ `${name} sprite` } />
+            <h2>{ name }</h2>
+            <h3>{ type }</h3>
+            <p>{ `Average Weight: ${averageWeight.value}${averageWeight.measurementUnit}` }</p>
+          </div>
+        )
+      }
+    </pokemonContext.Consumer>
+  );
 }
 
 export default Pokemon;
