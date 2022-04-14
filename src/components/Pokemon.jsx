aae0c22129cc5ffd899a489fad72d4cda5ecffaa
Douglas Souza <douglassouzabjj@hotmail.com>
2022-04-14 11:21:58 -0300

    Refatora componente Pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import pokemonContext from '../context/pokemonContext';
 
 function Pokemon() {
+  const { selectedPokemon: { image, name, type, averageWeight } } = useContext(pokemonContext);
+
   return (
-    <pokemonContext.Consumer>
-      {
-        ({ selectedPokemon: { image, name, type, averageWeight } }) => (
-          <div>
-            <img src={ image } alt={ `${name} sprite` } />
-            <h2>{ name }</h2>
-            <h3>{ type }</h3>
-            <p>{ `Average Weight: ${averageWeight.value}${averageWeight.measurementUnit}` }</p>
-          </div>
-        )
-      }
-    </pokemonContext.Consumer>
+    <div>
+      <img src={ image } alt={ `${name} sprite` } />
+      <h2>{ name }</h2>
+      <h3>{ type }</h3>
+      <p>{ `Average Weight: ${averageWeight.value}${averageWeight.measurementUnit}` }</p>
+    </div>
   );
 }
 
