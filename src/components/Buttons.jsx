155c6c1a114ebc5294dee66931eaeb434127d703
Douglas Souza <douglassouzabjj@hotmail.com>
2022-04-14 11:23:50 -0300

    Refatora componenete Buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import pokemonContext from '../context/pokemonContext';
 
 function Buttons() {
+  const { types, resetFilter, filterPokemons } = useContext(pokemonContext);
+
   return (
-    <pokemonContext.Consumer>
+    <section>
+      <button type="button" onClick={ resetFilter }>All</button>
       {
-        ({ types, resetFilter, filterPokemons }) => (
-          <section>
-            <button type="button" onClick={ resetFilter }>All</button>
-            {
-              types.map((type) => (
-                <button
-                  key={ type }
-                  onClick={ () => filterPokemons(type) }
-                >
-                  { type }
-                </button>
-              ))
-            }
-          </section>
-        )
-      }  
-    </pokemonContext.Consumer>
+        types.map((type) => (
+          <button
+            key={ type }
+            onClick={ () => filterPokemons(type) }
+          >
+            { type }
+          </button>
+        ))
+      }
+    </section>
   );
 }
 
