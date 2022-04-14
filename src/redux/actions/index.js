ce1350b96bbe6029443be121d0e895fd75a8d52c
Douglas Souza <douglassouzabjj@hotmail.com>
2022-04-14 09:07:49 -0300

    Remove pasta de redux

--- src/redux/actions/index.js
+++
@@ -1,9 +0,0 @@
-export const NEXT_POKEMON = 'NEXT_POKEMON';
-export const PREV_POKEMON = 'PREV_POKEMON';
-export const FILTER_POKEMONS = 'FILTER_POKEMONS';
-export const RESET_FILTER = 'RESET_FILTER';
-
-export const nextPokemon = () => ({ type: NEXT_POKEMON });
-export const prevPokemon = () => ({ type: PREV_POKEMON });
-export const filterPokemons = (filter) => ({ type: FILTER_POKEMONS, filter });
-export const resetFilter = () => ({ type: RESET_FILTER });
--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import pokemonsReducer from './pokemonsReducer';
-
-const rootReducer = combineReducers({
-  pokemons: pokemonsReducer,
-});
-
-export default rootReducer;
